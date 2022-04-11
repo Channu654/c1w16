87a420ea1e68ab22bedb6f088e0ecca732a458a6
channaveer <channaveerpatil999@gmail.com>
2022-04-11 13:15:08 +0530

    unit-4/evaluation

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -1,5 +1,24 @@
 // reusable card component
-const GroceryItem = () => {
-  return <></>;
+import React from "react";
+import './Grocery.css'
+
+const GroceryItem = (props) => {
+  console.log('props:', props)
+  const {id,title,imgURL,discount,sellingPrice,mrp}=props
+  
+  return(
+  <div id="main">
+        <img src={imgURL} alt="" />
+        <p>{title} </p>
+          <p>{discount} </p>
+          <p>{sellingPrice} </p>
+          <p>{mrp} </p>
+
+
+
+
+    </div>
+    
+    ) 
 };
 export default GroceryItem;
